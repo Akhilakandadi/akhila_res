76a5acfb50ec74a5d2663d1e9f5f9eadb86042b9
Akhilakandadi <akhilareddykandadi14@gmail.com>
2025-04-02 13:42:01 -0700

    Update urls.py

--- restaurant_complaint_system/urls.py
+++ restaurant_complaint_system/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('complaints/', include('complaints.urls')),
     path('', lambda request: redirect('login'), name='landing_page'),  # Redirect root URL to login page
 ]
-
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
